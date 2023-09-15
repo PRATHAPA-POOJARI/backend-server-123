08ee0838bbf3b307934c3223b991dffdee8972a2
prathap poojari <72817845+PRATHAPA-POOJARI@users.noreply.github.com>
2023-09-15 10:14:58 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,9 +2,10 @@ const express = require('express')
 const app = express()
 
 app.get("/home",(req,res)=>{
-    res.send("this is home page")
+    res.send("this is home pamnne")
 
 });
-app.listen(3000,()=>{
-    
-})
+app.listen(9000
+    , () => {
+    console.log('Server is running on port 9000');
+});
