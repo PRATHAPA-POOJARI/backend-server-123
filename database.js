55cf043c3f6dca76a07b074a2531b2db4e27115d
prathap poojari <72817845+PRATHAPA-POOJARI@users.noreply.github.com>
2023-10-03 23:27:14 +0530

    .env  package addes

--- database.js
+++ database.js
@@ -1,10 +1,13 @@
 const mongoose = require('mongoose'); // Added quotes around 'mongoose'
+
 const database = async () => {
     try {
-        await mongoose.connect("mongodb+srv://prathappoojari408:8495@cluster0.bvc5cg7.mongodb.net/?retryWrites=true&w=majority&appName=AtlasApp", {
+        await mongoose.connect(process.env.DATABASE_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 });
+
+        
         console.log('Database connected...');
     } catch (error) {
         console.error(error);
--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const userRouter= require('./router/userRouter')
 const database = require('./database'); // Corrected file name here
+require('dotenv').config()
 database()
 app.use(userRouter)
 app.listen(9000, () => {
