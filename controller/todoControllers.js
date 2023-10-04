a14b22b0f3abd4cd87de7f4db035799095c6a47f
prathap poojari <72817845+PRATHAPA-POOJARI@users.noreply.github.com>
2023-10-05 00:06:28 +0530

    Update todoControllers.js

--- controller/todoControllers.js
+++ controller/todoControllers.js
@@ -1,1 +1,10 @@
 const Todo = require("../models/TodoSchema")
+
+const postTodo=async(req,res)=>{
+    try {
+        
+    } catch (err ) {
+        console.log(err);
+    }
+}
+module.exports={postTodo}
