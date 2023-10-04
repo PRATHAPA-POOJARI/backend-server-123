824b591f0612402da10bb2e91523f7d78e374668
prathap poojari <72817845+PRATHAPA-POOJARI@users.noreply.github.com>
2023-10-04 19:00:53 +0530

    Update database.js

--- database.js
+++ database.js
@@ -1,14 +1,11 @@
 const mongoose = require('mongoose'); // Added quotes around 'mongoose'
-
 const database = async () => {
     try {
         await mongoose.connect(process.env.DATABASE_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 });
-
-        
-        console.log('Database connected...');
+    console.log('Database connected...');
     } catch (error) {
         console.error(error);
     }
