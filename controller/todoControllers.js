fc86b5deea4ecf577f54951ab6e3c91fe65b0187
prathap poojari <72817845+PRATHAPA-POOJARI@users.noreply.github.com>
2023-10-07 10:14:43 +0530

    create api

--- controller/todoControllers.js
+++ controller/todoControllers.js
@@ -2,9 +2,11 @@ const Todo = require("../models/TodoSchema")
 
 const postTodo=async(req,res)=>{
     try {
-        
+         const  success= await Todo.create({todo:"this is the todo"})
     } catch (err ) {
         console.log(err);
     }
 }
 modul.exports={postTodo}
+
+ 
