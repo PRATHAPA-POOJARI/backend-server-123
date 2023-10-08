ae8587388f7e29616be020274cc51443653e7891
prathap poojari <72817845+PRATHAPA-POOJARI@users.noreply.github.com>
2023-10-08 09:46:22 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const app = express()
 const userRouter= require('./router/userRouter')
 const database = require('./database'); // Corrected file name here
 require('dotenv').config()
+
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
 database()
 app.use(userRouter)
 app.listen(9000, () => {
