0b8460f46ea1bd19dbc4f50b10659b12318ec6f4
prathap poojari <72817845+PRATHAPA-POOJARI@users.noreply.github.com>
2023-10-10 23:45:11 +0530

    Update userRouter.js

--- router/userRouter.js
+++ router/userRouter.js
@@ -1,11 +1,14 @@
 const express= require('express');
-const{postTodo}=require('../controller/todoControllers');
+const{postTodo, updateTodo, getTodo}=require('../controller/todoControllers');
 const router= express.Router();
 
 
 // const getUser=require("../controller/useConroller")
 // router.get("/home",getUser) 
 
-router.post('/postTodo/:todo',postTodo)
+router.post('/posttodo/:todo',postTodo)
+router.put('/updatetodo/:id/:updatedtodo',updateTodo)
+// router.delete('/tododeleted/:id', deleteTodo)
+router.get('/gettodo',getTodo)
 
 module.exports=router 
