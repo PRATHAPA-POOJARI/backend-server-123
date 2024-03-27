71368326cf97ad83f02980481b391f8177e573af
prathap poojari <72817845+PRATHAPA-POOJARI@users.noreply.github.com>
2024-03-27 23:53:06 +0530

    Update todoControllers.js

--- controller/todoControllers.js
+++ controller/todoControllers.js
@@ -12,6 +12,45 @@ const postTodo=async(req,res)=>{
         console.log(err);
     }
 }
-module.exports={postTodo}
+
+const updateTodo=async(req,res)=>{
+    try {
+         console.log(req.params.updatedtodo)
+         const  success= await Todo.findByIdAndUpdate(req.params.id,{todo:req.params.updatedtodo})
+         if(success){
+             res.status(200).send({"message":"Todo updated..."})
+         }
+         res.status(400).send({"message":"Not  not updated..."})
+    } catch (err ) {
+        console.log(err);
+    }
+}
+
+const deleteTodo=async(req,res)=>{
+    try {
+         
+         const  success= await Todo.findByIdAndDelete(req.params.id)
+         if(success){
+             res.status(200).send({"message":"Todo Deleted..."})
+         }
+         res.status(400).send({"message":"Not  not Delete..."})
+    } catch (err ) {
+        console.log(err);
+    }
+}
+
+const getTodo=async(req,res)=>{
+    try {
+         
+         const  success= await Todo.find()
+         if(success){
+             res.status(200).send({ success,"message":"ok..."})
+         }
+         res.status(400).send({"message":"Not  not ok..."})
+    } catch (err ) {
+        console.log(err);
+    }
+}
+module.exports={postTodo, updateTodo,deleteTodo, getTodo}
 
  
